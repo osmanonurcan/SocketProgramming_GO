bb6152d4a15281f89cf2ce38449c76fa00b55501
osmanonurcan <osmanonurcan72@gmail.com>
2022-04-09 13:39:53 +0300

    folder organazing

---
+++ go_client/src/main/java/game/Message.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package game;
+
+/**
+ *
+ * @author osman
+ */
+public class Message implements java.io.Serializable {
+    public static enum Message_Type {None, Name, Disconnect,RivalConnected, Text, Selected, Bitis,Start,}
+    
+    public Message_Type type;
+    public Object content;
+    public Message(Message_Type t)
+    {
+        this.type=t;
+    }
+ 
+
+    
+    
+}
